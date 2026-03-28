3507fb07740caccda4bda1c80ba4ef155a81c373
Buffrr <contact@buffrr.dev>
2026-03-28 16:38:03 +0100

    Use signingId helper

--- fabric/js/fabric-web/src/signing.ts
+++ fabric/js/fabric-web/src/signing.ts
@@ -1,20 +1,18 @@
 export { signMessage, verifyMessage } from "@spacesprotocol/fabric-core/signing";
-export { signRecords as signRecordsRaw } from "@spacesprotocol/fabric-core/signing";
-import { signRecords as _signRecords } from "@spacesprotocol/fabric-core/signing";
+import { schnorr } from "@noble/curves/secp256k1";
 import * as libveritas from "@spacesprotocol/libveritas";
 
 /**
  * Sign a record set and produce OffchainRecords bytes.
  *
- * @param recordSetBytes - Serialized record set bytes (e.g. `recordSet.toBytes()`)
+ * @param recordSet - A RecordSet object (from `RecordSet.pack()`)
  * @param secretKey - 32-byte BIP-340 secret key
  * @returns OffchainRecords bytes ready for `fabric.publish()`
  */
 export function signRecords(
-  recordSetBytes: Uint8Array,
+  recordSet: { signingId(): Uint8Array; toBytes(): Uint8Array },
   secretKey: Uint8Array,
 ): Uint8Array {
-  return _signRecords(recordSetBytes, secretKey, (rs, sig) =>
-    new (libveritas as any).OffchainRecords(rs, sig).toBytes()
-  );
+  const sig = schnorr.sign(recordSet.signingId(), secretKey);
+  return (libveritas as any).createOffchainRecords(recordSet.toBytes(), sig);
 }
